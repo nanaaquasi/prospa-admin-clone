cdc8c0846dfc94cfd965ed2315ae38265d1d73ee
nanaaquasi <nanaaquasi5@gmail.com>
2021-05-25 13:03:55 +0000

    add mock data

---
+++ src/utils/mock.js
@@ -0,0 +1,478 @@
+export const dialCodes = [
+  {
+    name: "Ghana",
+    dail_code: "+233",
+    code: "GH",
+  },
+
+  {
+    name: "Mozambique",
+    dail_code: "+258",
+    code: "MZ",
+  },
+  {
+    name: "Nicaragua",
+    dail_code: "+505",
+    code: "NI",
+  },
+  {
+    name: "Niger",
+    dail_code: "+227",
+    code: "NE",
+  },
+  {
+    name: "Nigeria",
+    dail_code: "+234",
+    code: "NG",
+  },
+  {
+    name: "Niue",
+    dail_code: "+683",
+    code: "NU",
+  },
+  {
+    name: "Norfolk Island",
+    dail_code: "+672",
+    code: "NF",
+  },
+  {
+    name: "Northern Mariana Islands",
+    dail_code: "+1670",
+    code: "MP",
+  },
+  {
+    name: "Norway",
+    dail_code: "+47",
+    code: "NO",
+  },
+  {
+    name: "Oman",
+    dail_code: "+968",
+    code: "OM",
+  },
+  {
+    name: "Pakistan",
+    dail_code: "+92",
+    code: "PK",
+  },
+  {
+    name: "Palau",
+    dail_code: "+680",
+    code: "PW",
+  },
+  {
+    name: "Palestinian Territory, Occupied",
+    dail_code: "+970",
+    code: "PS",
+  },
+  {
+    name: "Panama",
+    dail_code: "+507",
+    code: "PA",
+  },
+  {
+    name: "Papua New Guinea",
+    dail_code: "+675",
+    code: "PG",
+  },
+  {
+    name: "Paraguay",
+    dail_code: "+595",
+    code: "PY",
+  },
+  {
+    name: "Peru",
+    dail_code: "+51",
+    code: "PE",
+  },
+  {
+    name: "Philippines",
+    dail_code: "+63",
+    code: "PH",
+  },
+  {
+    name: "Pitcairn",
+    dail_code: "+872",
+    code: "PN",
+  },
+  {
+    name: "Poland",
+    dail_code: "+48",
+    code: "PL",
+  },
+  {
+    name: "Portugal",
+    dail_code: "+351",
+    code: "PT",
+  },
+  {
+    name: "Puerto Rico",
+    dail_code: "+1939",
+    code: "PR",
+  },
+  {
+    name: "Qatar",
+    dail_code: "+974",
+    code: "QA",
+  },
+  {
+    name: "Romania",
+    dail_code: "+40",
+    code: "RO",
+  },
+  {
+    name: "Russia",
+    dail_code: "+7",
+    code: "RU",
+  },
+  {
+    name: "Rwanda",
+    dail_code: "+250",
+    code: "RW",
+  },
+  {
+    name: "Reunion",
+    dail_code: "+262",
+    code: "RE",
+  },
+  {
+    name: "Saint Barthelemy",
+    dail_code: "+590",
+    code: "BL",
+  },
+  {
+    name: "Saint Helena, Ascension and Tristan Da Cunha",
+    dail_code: "+290",
+    code: "SH",
+  },
+  {
+    name: "Saint Kitts and Nevis",
+    dail_code: "+1869",
+    code: "KN",
+  },
+  {
+    name: "Saint Lucia",
+    dail_code: "+1758",
+    code: "LC",
+  },
+  {
+    name: "Saint Martin",
+    dail_code: "+590",
+    code: "MF",
+  },
+  {
+    name: "Saint Pierre and Miquelon",
+    dail_code: "+508",
+    code: "PM",
+  },
+  {
+    name: "Saint Vincent and the Grenadines",
+    dail_code: "+1784",
+    code: "VC",
+  },
+  {
+    name: "Samoa",
+    dail_code: "+685",
+    code: "WS",
+  },
+  {
+    name: "San Marino",
+    dail_code: "+378",
+    code: "SM",
+  },
+  {
+    name: "Sao Tome and Principe",
+    dail_code: "+239",
+    code: "ST",
+  },
+  {
+    name: "Saudi Arabia",
+    dail_code: "+966",
+    code: "SA",
+  },
+  {
+    name: "Senegal",
+    dail_code: "+221",
+    code: "SN",
+  },
+  {
+    name: "Serbia",
+    dail_code: "+381",
+    code: "RS",
+  },
+  {
+    name: "Seychelles",
+    dail_code: "+248",
+    code: "SC",
+  },
+  {
+    name: "Sierra Leone",
+    dail_code: "+232",
+    code: "SL",
+  },
+  {
+    name: "Singapore",
+    dail_code: "+65",
+    code: "SG",
+  },
+  {
+    name: "Slovakia",
+    dail_code: "+421",
+    code: "SK",
+  },
+  {
+    name: "Slovenia",
+    dail_code: "+386",
+    code: "SI",
+  },
+  {
+    name: "Solomon Islands",
+    dail_code: "+677",
+    code: "SB",
+  },
+  {
+    name: "Somalia",
+    dail_code: "+252",
+    code: "SO",
+  },
+  {
+    name: "South Africa",
+    dail_code: "+27",
+    code: "ZA",
+  },
+  {
+    name: "South Sudan",
+    dail_code: "+211",
+    code: "SS",
+  },
+  {
+    name: "South Georgia and the South Sandwich Islands",
+    dail_code: "+500",
+    code: "GS",
+  },
+  {
+    name: "Spain",
+    dail_code: "+34",
+    code: "ES",
+  },
+  {
+    name: "Sri Lanka",
+    dail_code: "+94",
+    code: "LK",
+  },
+  {
+    name: "Sudan",
+    dail_code: "+249",
+    code: "SD",
+  },
+  {
+    name: "Suriname",
+    dail_code: "+597",
+    code: "SR",
+  },
+  {
+    name: "Svalbard and Jan Mayen",
+    dail_code: "+47",
+    code: "SJ",
+  },
+  {
+    name: "Swaziland",
+    dail_code: "+268",
+    code: "SZ",
+  },
+  {
+    name: "Sweden",
+    dail_code: "+46",
+    code: "SE",
+  },
+  {
+    name: "Switzerland",
+    dail_code: "+41",
+    code: "CH",
+  },
+  {
+    name: "Syrian Arab Republic",
+    dail_code: "+963",
+    code: "SY",
+  },
+  {
+    name: "Taiwan",
+    dail_code: "+886",
+    code: "TW",
+  },
+  {
+    name: "Tajikistan",
+    dail_code: "+992",
+    code: "TJ",
+  },
+  {
+    name: "Tanzania, United Republic of Tanzania",
+    dail_code: "+255",
+    code: "TZ",
+  },
+  {
+    name: "Thailand",
+    dail_code: "+66",
+    code: "TH",
+  },
+  {
+    name: "Timor-Leste",
+    dail_code: "+670",
+    code: "TL",
+  },
+  {
+    name: "Togo",
+    dail_code: "+228",
+    code: "TG",
+  },
+  {
+    name: "Tokelau",
+    dail_code: "+690",
+    code: "TK",
+  },
+  {
+    name: "Tonga",
+    dail_code: "+676",
+    code: "TO",
+  },
+  {
+    name: "Trinidad and Tobago",
+    dail_code: "+1868",
+    code: "TT",
+  },
+  {
+    name: "Tunisia",
+    dail_code: "+216",
+    code: "TN",
+  },
+  {
+    name: "Turkey",
+    dail_code: "+90",
+    code: "TR",
+  },
+  {
+    name: "Turkmenistan",
+    dail_code: "+993",
+    code: "TM",
+  },
+  {
+    name: "Turks and Caicos Islands",
+    dail_code: "+1649",
+    code: "TC",
+  },
+  {
+    name: "Tuvalu",
+    dail_code: "+688",
+    code: "TV",
+  },
+  {
+    name: "Uganda",
+    dail_code: "+256",
+    code: "UG",
+  },
+  {
+    name: "Ukraine",
+    dail_code: "+380",
+    code: "UA",
+  },
+  {
+    name: "United Arab Emirates",
+    dail_code: "+971",
+    code: "AE",
+  },
+  {
+    name: "United Kingdom",
+    dail_code: "+44",
+    code: "GB",
+  },
+  {
+    name: "United States",
+    dail_code: "+1",
+    code: "US",
+  },
+  {
+    name: "Uruguay",
+    dail_code: "+598",
+    code: "UY",
+  },
+  {
+    name: "Uzbekistan",
+    dail_code: "+998",
+    code: "UZ",
+  },
+  {
+    name: "Vanuatu",
+    dail_code: "+678",
+    code: "VU",
+  },
+  {
+    name: "Venezuela, Bolivarian Republic of Venezuela",
+    dail_code: "+58",
+    code: "VE",
+  },
+  {
+    name: "Vietnam",
+    dail_code: "+84",
+    code: "VN",
+  },
+  {
+    name: "Virgin Islands, British",
+    dail_code: "+1284",
+    code: "VG",
+  },
+  {
+    name: "Virgin Islands, U.S.",
+    dail_code: "+1340",
+    code: "VI",
+  },
+  {
+    name: "Wallis and Futuna",
+    dail_code: "+681",
+    code: "WF",
+  },
+  {
+    name: "Yemen",
+    dail_code: "+967",
+    code: "YE",
+  },
+  {
+    name: "Zambia",
+    dail_code: "+260",
+    code: "ZM",
+  },
+  {
+    name: "Zimbabwe",
+    dail_code: "+263",
+    code: "ZW",
+  },
+];
+
+export const accountTypes = [
+  {
+    title: "I have a registered business / charity with CAC",
+    description: "As a registered business you will get:",
+    types: [
+      "Account in your business name",
+      "Send and receive transfers from all Nigerian banks",
+      "Tools for managing your business",
+    ],
+  },
+  {
+    title: "My business is not yet registered, I would like to register",
+    description: "What you will get:",
+    types: [
+      "Registered business name with the CAC & certificate",
+      "Tax identification number",
+      "Your account will be automatically opened on completion",
+    ],
+  },
+  {
+    title: "I am a freelancer, I do business in my personal name",
+    description: "As a freelancer you'll get:",
+    types: [
+      "Account in your personal name",
+      "Send and receive transfers from all Nigerian banks",
+      "Tools for managing your business",
+    ],
+  },
+];

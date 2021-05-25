687e4f4bec24f1d6cc1c3922fa1f11841b158cb3
nanaaquasi <nanaaquasi5@gmail.com>
2021-05-25 17:29:10 +0000

    design authlayout

--- src/layouts/AuthLayout.js
+++ src/layouts/AuthLayout.js
@@ -4,54 +4,61 @@ import { Link, useHistory, useParams } from "react-router-dom";
 import StoryBar from "../components/StoryBar";
 import AuthImage from "../assets/img/auth-img.png";
 import AuthNavigator from "../navigation/AuthNavigator";
+import { useWindowDimensions } from "../utils/hooks";
+import logo from "../assets/img/logo.svg";
 
 const AuthLayout = (props) => {
+  const { width } = useWindowDimensions();
+
+  const isMobile = width < 600;
   return (
     <Box d="flex" w="100%" h="100%" flexDirection={["column", "row"]}>
-      <Flex
-        direction="column"
-        height="100%"
-        overflowY="hidden"
-        w={["100%", "23.5%"]}
-        h={["200px", "100vh"]}
-        bg="#7d51fe"
-        p="8"
-      >
-        <Link to="/">
-          <Box mb="14">
-            <Image src="https://getprospa.com/static/icons/prospa-logo2.svg" />
-          </Box>
-        </Link>
+      {!isMobile && (
+        <Flex
+          direction="column"
+          height="100%"
+          overflowY="hidden"
+          w={["100%", "23.5%"]}
+          h={["200px", "100vh"]}
+          bg={props.bg}
+          p="8"
+        >
+          <Link to="/">
+            <Box mb="14">
+              <Image src={logo} alt="Prospa Logo" />
+            </Box>
+          </Link>
 
-        <StoryBar />
-        <Stack spacing="4" my="10">
-          <Text
-            fontSize="32px"
-            fontWeight="bold"
-            color="#feffff"
-            lineHeight="1"
-            // w="80%"
-          >
-            Create Multiple sub-accounts
+          <StoryBar />
+          <Stack spacing="4" my="10">
+            <Text
+              fontSize="32px"
+              fontWeight="bold"
+              color="#feffff"
+              lineHeight="1"
+              // w="80%"
+            >
+              Create Multiple sub-accounts
+            </Text>
+            <Text
+              color="#feffff"
+              lineHeight="1.2"
+              fontSize="14px"
+              fontWeight="300"
+              lineHeight="20px"
+            >
+              Organise your business finances easily with multiple accounts. No
+              limits
+            </Text>
+          </Stack>
+          <Box w="250px" h="250px" mb="14">
+            <Image w="100%" h="100%" src={AuthImage} alt="Auth Image" />
+          </Box>
+          <Text fontSize="14px" mt="20" color="#feffff">
+            © {new Date().getFullYear()} Prospa Inc
           </Text>
-          <Text
-            color="#feffff"
-            lineHeight="1.2"
-            fontSize="14px"
-            fontWeight="300"
-            lineHeight="20px"
-          >
-            Organise your business finances easily with multiple accounts. No
-            limits
-          </Text>
-        </Stack>
-        <Box w="250px" h="250px" mb="14">
-          <Image w="100%" h="100%" src={AuthImage} alt="Auth Image" />
-        </Box>
-        <Text fontSize="14px" mt="20" color="#feffff">
-          © {new Date().getFullYear()} Prospa Inc
-        </Text>
-      </Flex>
+        </Flex>
+      )}
 
       <Flex
         w={["100%", "76.5%"]}
